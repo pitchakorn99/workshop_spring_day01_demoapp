b32b67dd3683acfad983c5304facf369d7f42129
KBANKNET\pitchakorn.jo <pitchakorn.jo@kbtg.tech>
2021-09-06 16:18:41 +0700

    Create Spring Web MVC Test

---
+++ src/test/java/com/example/demoapp/employees/EmployeeControllerMvcTest.java
@@ -0,0 +1,14 @@
+package com.example.demoapp.employees;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@WebMvcTest(EmployeeController.class)
+class EmployeeControllerMvcTest {
+    @Test
+    public void getEmployeeById(){
+
+    }
+}
