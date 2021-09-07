5f7473e1f5d0bbb9c4a9e276b79b561f25d788e4
KBANKNET\pitchakorn.jo <pitchakorn.jo@kbtg.tech>
2021-09-07 10:09:00 +0700

    Create test find employee id with string data JPA

--- src/main/java/com/example/demoapp/employees/Employee.java
+++ src/main/java/com/example/demoapp/employees/Employee.java
@@ -11,7 +11,7 @@ import javax.persistence.*;
 //@Table(name = "tbl_employee")
 public class Employee {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
 }
--- src/main/java/com/example/demoapp/employees/EmployeeRepository.java
+++ src/main/java/com/example/demoapp/employees/EmployeeRepository.java
@@ -1,7 +1,12 @@
 package com.example.demoapp.employees;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
+    //@Query("select * from employee")
+    //List<Employee> findAllByIdAndName(int id, String name);
 }
